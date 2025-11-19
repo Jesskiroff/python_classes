1a8a8c62478e57888d362b42bdfeea84cef944ee
Jessica Kiroff <jesskiroff@gmail.com>
2025-11-19 18:37:22 -0500

    add user id

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 class User: #python classes are written in PascalCase
-    pass 
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        
+user_1 = User("001 ", "Jess")
+
+print(user_1.id)

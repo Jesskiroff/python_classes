94b302c5663701c3df5f5f8cb66a8a2e5a2b6b29
Jessica Kiroff <jesskiroff@gmail.com>
2025-11-19 19:01:29 -0500

    add follow class

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ class User: #python classes are written in PascalCase
     def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
         
 user_1 = User("001 ", "Jess")
+user_2 = User("002", "Jill")
+
+user_1.follow(user_2)
+
 
 print(user_1.id)
+print(user_2.username)
+
+print(user_2.followers)
+print(user_1.following)
+print(user_1.followers)
+print(user_2.following)
